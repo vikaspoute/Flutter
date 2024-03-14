18c3e1bf616ae443dd73ef5e277fb8279ea7e456
vicky_07 <vikas.musterdekho@gmail.com>
2024-03-14 08:01:07 +0530

    login screen

---
+++ flutter_bloc/login_form/lib/bloc/auth_bloc.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthInitial()) {
+    on<AuthLoginRequested>(_onAuthLoginRequested);
+    on<AuthLogoutRequested>(_onAuthLogoutRequested);
+  }
+  void _onAuthLoginRequested(
+    AuthLoginRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      final email = event.email;
+      final password = event.password;
+      // Email validation using Regex
+
+      if (password.length < 6) {
+        return emit(
+          AuthFailure('Password cannot be less than 6 characters!'),
+        );
+      }
+
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthSuccess(uid: '$email-$password'));
+      });
+    } catch (e) {
+      return emit(AuthFailure(e.toString()));
+    }
+  }
+
+  void _onAuthLogoutRequested(
+    AuthLogoutRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthInitial());
+      });
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
+  }
+}
